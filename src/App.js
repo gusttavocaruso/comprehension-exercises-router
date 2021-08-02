ff010dc21f9d3b715a0636385999b484897ceb21
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-02 13:03:35 -0300

    add sixth requirement solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
@@ -9,6 +9,11 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> <br />
+          <Link to="/about">About</Link> <br />
+          <Link to="/users">Users</Link> <br />
+        </nav>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
         <Route path="/users" component={ Users } />
