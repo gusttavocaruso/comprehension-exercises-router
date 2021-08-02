3ebf1729f9e5804897ab35f6407fe1982f2110ba
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-02 12:57:45 -0300

    4th and 5th requirement solved

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Home } />
+        <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
+        <Route path="/users" component={ Users } />
       </BrowserRouter>
     );
   }
