9e4135a3664c88d83751407040c600af74c04a2d
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-02 12:52:50 -0300

    tird requirement solved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Home from './components/Home';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" component={ Home } />
+        <Route path="/about" component={ About } />
       </BrowserRouter>
     );
   }
